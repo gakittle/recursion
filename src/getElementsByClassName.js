0ef1f52ea2961f24eba4eb821360a786b57ac8c0
Greg Kittleson <greg.kittleson@gmail.com>
2018-10-12 15:09:08 -0700

    getElementsByClassName passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,33 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  // document.body, element.childNodes, and element.classList
+
+  // input: className
+  //elementsByClass = [];
+  // select document.body
+
+  // for some random element, do the following:
+  	// for each of that element's children:
+  		// if element.classList _.contains className
+  		  // elementsByClass.push(element)
+  		// return elementsByClass.concat(other call of function(element.childNodes));
+  // output: HTML collection (array?) of <div>.className
+
+
+  // base case: no child nodes or element does not have class in classList
+
+  elementsByClass = [];
+  var htmlCollect = function (el) {
+  	var classes = el.classList;
+  	if (_.contains(classes, className)) {
+  		elementsByClass.push(el);
+  	}
+  	var children = el.childNodes;
+  	children.forEach(function(node) {
+  		return elementsByClass.concat(htmlCollect(node));
+  	});
+  };
+  htmlCollect(document.body);
+  return elementsByClass;
 };
